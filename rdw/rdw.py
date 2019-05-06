f736d26ec836ae2471f202f842196e524ebaf003
eelcohn <eelco@huininga.nl>
2019-05-06 18:35:59 +0000

    Update rdw.py

--- rdw/rdw.py
+++ rdw/rdw.py
@@ -8,19 +8,22 @@ VERSION = '3.0.0'
 from datetime import datetime, timedelta
 import requests
 
-_RESOURCE_GEKENTEKENDE_VOERTUIGEN = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken={}'
-_RESOURCE_TERUGROEP_ACTIE = 'https://opendata.rdw.nl/resource/af5r-44mf.json?referentiecode_rdw={}'
-_RESOURCE_TERUGROEP_ACTIE_STATUS = 'https://opendata.rdw.nl/resource/t49b-isb7.json?kenteken={}'
-_RESOURCE_TERUGROEP_ACTIE_RISICO = 'https://opendata.rdw.nl/resource/9ihi-jgpf.json?referentiecode_rdw={}'
 
-
-
-class RDWException(Exception):
+class RdwException(Exception):
     pass
 
-
                 
-class RDW(object):
+class Rdw(object):
+    _RDW_API_BASE_URL = 'https://opendata.rdw.nl/resource/{}.json?'
+    _RDW_API_GEBREKEN = 'tbph-ct3j'
+    _RDW_API_GECONSTATEERDE_GEBREKEN = '2u8a-sfar'
+    _RDW_API_GEKENTEKENDE_VOERTUIGEN = 'm9d7-ebf2'
+    _RDW_API_TERUGROEP_ACTIE = 'af5r-44mf'
+    _RDW_API_TERUGROEP_ACTIE_RISICO = '9ihi-jgpf'
+    _RDW_API_TERUGROEP_ACTIE_STATUS = 't49b-isb7'
+    _RDW_API_TERUGROEP_INFORMEREN_EIGENAAR = '223d-3w9w'
+    _RDW_API_TOEGEVOEGDE_OBJECTEN = '5bwx-4xqb'
+
     """
     Interface class for the RDW API's.
     """
@@ -29,114 +32,117 @@ class RDW(object):
     def __init__(self, plate_id):
         """
         Initiates the sensor data with default settings if none other are set.
-        :param plate: license plate id
+        :param plate_id: license plate id
         """
         self._plate_id = plate_id
         self._session = Session()
 
-    def get_apk_data(self, referencecode):
+    def get_deficiency_data(self, gebrek_identificatie):
         """
-        Get data from the RDW APK API
+        Get data from the RDW Gebreken API
+        :param gebrek_identificatie: RDW deficiency identification code
         :return: A JSON list containing the RDW APK data
         """
 
-        try:
-            result = self._session.get(_RESOURCE_GEKENTEKENDE_VOERTUIGEN.format(plate_id), data="json={}")
-        except:
-            raise(RDWException("RDW: Unable to connect to the RDW Recall API"))
-            return None
+        return self._get_rdwapi_data(_RDW_API_GEBREKEN, 'gebrek_identificatie={}'.format(gebrek_identificatie))
 
-        self._current_status_code = result.status_code
-
-        if self._current_status_code != 200:
-            raise(RDWException("RDW: Got an invalid HTTP status code %s from RDW APK API", self._current_status_code))
-            return None
-
-        _LOGGER.debug("RDW: raw APK data: %s", result)
-
-        try:
-            data = result.json()[0]
-        except:
-            raise(RDWException("RDW: Got invalid response from RDW APK API. Is the license plate id %s correct?", plate_id))
-            data = None
-
-        return data
-
-    def get_recall_data(self, referencecode):
+    def get_found_deficiencies_data(self, kenteken):
         """
-        Get data from the RDW Recall API
-        :return: A JSON list containing the RDW Recall data
+        Get data from the RDW Geconstateerde Gebreken API
+        :param kenteken: License plate ID
+        :return: A JSON list containing the RDW APK data
         """
 
-        try:
-            result = self._session.get(_RESOURCE_TERUGROEP_ACTIE.format(referencecode), data="json={}")
-        except:
-            raise(RDWException("RDW: Unable to connect to the RDW Recall API"))
-            return None
+        return self._get_rdwapi_data(_RDW_API_GECONSTATEERDE_GEBREKEN, 'kenteken={}'.format(kenteken))
 
-        self._current_status_code = result.status_code
-
-        if self._current_status_code != 200:
-            raise(RDWException("RDW: Got an invalid HTTP status code %s from RDW Recall API", self._current_status_code))
-            return None
-
-        try:
-            data = result.json()
-        except:
-            raise(RDWException("RDW: Got invalid response from RDW Recall API. Is the reference code %s correct?", referencecode))
-            data = None
-
-        return data
-
-    def get_recall_status_data(self, plate_id):
+    def get_vehicle_data(self, kenteken):
         """
-        Get data from the RDW Recall API
-        :return: A JSON list containing the RDW Recall data
+        Get data from the RDW Gekentekende Voertuigen API
+        :param kenteken: License plate ID
+        :return: A JSON list containing the RDW APK data
         """
 
-        try:
-            result = self._session.get(_RESOURCE_TERUGROEP_ACTIE_STATUS.format(plate_id), data="json={}")
-        except:
-            raise(RDWException("RDW: Unable to connect to the RDW Recall Status API"))
-            return None
+        return self._get_rdwapi_data(_RDW_API_GEKENTEKENDE_VOERTUIGEN, 'kenteken={}'.format(kenteken))
 
-        self._current_status_code = result.status_code
-
-        if self._current_status_code != 200:
-            raise(RDWException("RDW: Got an invalid HTTP status code %s from RDW Recall Status API", self._current_status_code))
-            return None
-
-        try:
-            data = result.json()
-        except:
-            raise(RDWException("RDW: Got invalid response from RDW Recall Status API. Is the license plate id %s correct?", plate_id))
-            data = None
-
-        return data
-
-    def get_recall_risk(self, plate_id):
+    def get_recall_data(self, referentiecode_rdw):
         """
-        Get data from the RDW Recall Risk API
+        Get data from the RDW Terugroep Actie API
+        :param referentiecode_rdw: RDW reference code
         :return: A JSON list containing the RDW Recall Risk data
         """
 
+        return self._get_rdwapi_data(_RDW_API_TERUGROEP_ACTIE, 'referentiecode_rdw={}'.format(referentiecode_rdw))
+
+    def get_recall_risk_data(self, referentiecode_rdw):
+        """
+        Get data from the RDW Terugroep Actie Risico API
+        :param referentiecode_rdw: RDW reference code
+        :return: A JSON list containing the RDW Recall Risk data
+        """
+
+        return self._get_rdwapi_data(_RDW_API_TERUGROEP_ACTIE_RISICO, 'referentiecode_rdw={}'.format(referentiecode_rdw))
+
+    def get_recall_status_data(self, kenteken, referentiecode_rdw):
+        """
+        Get data from the RDW Terugroep Actie Status API
+        :param kenteken: License plate ID
+        :param referentiecode_rdw: RDW reference code
+        :return: A JSON list containing the RDW Recall Risk data
+        """
+
+        return self._get_rdwapi_data(_RDW_API_TERUGROEP_ACTIE_STATUS, 'kenteken={}&referentiecode_rdw={}'.format(kenteken, referentiecode_rdw))
+
+    def get_recall_owner_notification(self, referentiecode_rdw):
+        """
+        Get data from the RDW Terugroep Informeren Eigenaar API
+        :param referentiecode_rdw: RDW reference code
+        :return: A JSON list containing the RDW Recall Risk data
+        """
+
+        return self._get_rdwapi_data(_RDW_API_TERUGROEP_INFORMEREN_EIGENAAR, 'referentiecode_rdw={}'.format(referentiecode_rdw))
+
+    def get_added_objects_data(self, kenteken):
+        """
+        Get data from the RDW Toegevoegde Objecten API
+        :param kenteken: License plate ID
+        :return: A JSON list containing the RDW APK data
+        """
+
+        return self._get_rdwapi_data(_RDW_API_TOEGEVOEGDE_OBJECTEN, 'kenteken={}'.format(kenteken))
+
+    def _get_rdwapi_data(self, endpoint, query):
+        """
+        Get data from one of the RDW API endpoints
+        :param endpoint: RDW endpoint
+        :param query: Query to send to the endpoint
+        :return: A JSON list containing the RDW data
+        """
+
+        url = _RDWAPI_BASEURL.format(endpoint) + query
+
         try:
-            result = self._session.get(_RESOURCE_TERUGROEP_ACTIE_STATUS.format(plate_id), data="json={}")
+            result = self._session.get(url, data="json={}")
         except:
-            raise(RDWException("RDW: Unable to connect to the RDW Recall Risk API"))
+            raise(RdwException("RDW: Unable to connect to RDW API endpoint %s", endpoint))
             return None
 
+        """ Check if the RDW API webserver returned a valid HTTP status code """
         self._current_status_code = result.status_code
 
         if self._current_status_code != 200:
-            raise(RDWException("RDW: Got an invalid HTTP status code %s from RDW Recall Risk API", self._current_status_code))
+            raise(RdwException("RDW: Got an invalid HTTP status code %s from RDW API endpoint %s", self._current_status_code, endpoint))
             return None
 
+        """ Check if the RDW API returned valid JSON data """
         try:
             data = result.json()
         except:
-            raise(RDWException("RDW: Got invalid response from RDW Recall Risk API. Is the license plate id %s correct?", plate_id))
+            raise(RdwException("RDW: Got invalid response from RDW API endpoint %s. Is the query %s correct?", endpoint, query))
             data = None
 
+        """ Check if the RDW API returned an error """
+        if data.error:
+            raise(RdwException("RDW: RDW API endpoint %s returned an error: %s", endpoint, data.message))
+            data = None            
+            
         return data
-
